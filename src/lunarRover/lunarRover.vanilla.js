308038e5bb7527c5d72ba46fad41878aa4f34957
Xavier Fuentes <>
2017-08-02 11:10:56 +0100

    upgraded to create-react-app

---
+++ src/lunarRover/lunarRover.vanilla.js
@@ -0,0 +1,9 @@
+import './lunarRover.vanilla.css';
+
+// Model the Moon, Lander and Rover as separate objects and export them
+
+// The Lander can land on the Moon
+// The Lander will land at a specific position
+// The Rover can disembark the Lander
+// The Rover will disembark initially at the Landers position
+// The Rover can move around the Moon
